ebc1743ed040a49c0b6c9db75bd39f9fdede892a
habibly <61365368+habibly@users.noreply.github.com>
2024-02-25 17:30:28 +0500

    remove unwanted files

--- src/components/Stage.js
+++
@@ -1,9 +0,0 @@
-function Stage() {
-  // show existing timers
-  // add timer
-  // delete timer
-  // show new timer on initialization if no existing timers
-
-
-}
-export  default Stage;
--- src/components/Timer.js
+++
@@ -1,9 +0,0 @@
-function Timer() {
-  // show countdown
-  // show label if present else show the total time
-  // pause timer
-  // play timer
-
-
-}
-export  default Timer;
